57c7dfc4cfff247e97a5613d288e12645a607568
Chawanakorn-Wongtaso <patchawanakorn@gmail.com>
2024-10-29 17:38:22 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class=""></div>
+</template>
+
+<script setup lang="ts">
+// ------------- import components ------------- //
+
+// ------------- import components ------------- //
+
+// -------------- import libraries ------------- //
+
+// -------------- import libraries ------------- //
+
+// ----------------- variables ----------------- //
+
+// ----------------- variables ----------------- //
+
+// ----------------- functions ----------------- //
+
+// ----------------- functions ----------------- //
+</script>
+
+<style lang="scss"></style>
+
+<style lang="scss" scoped></style>
